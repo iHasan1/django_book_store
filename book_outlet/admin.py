49363589168947cca1da161c16550bda7fb01e9f
JBS <jbs@JBSs-MacBook-Pro.local>
2025-11-10 04:17:56 +0500

    removed save method from models and added Book to Admin

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Book
+
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    # readonly_fields = ("slug",)
+    prepopulated_fields = {
+        "slug": ("title",)
+    }
+    pass
+
+admin.site.register(Book, BookAdmin)
